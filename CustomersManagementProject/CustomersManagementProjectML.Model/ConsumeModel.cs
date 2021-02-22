137e348f64454f91409c861c5742f80346bde31b
naorb <naorbarkochva@gmail.com>
2021-02-23 01:59:43 +0200

    PDF fix

--- CustomersManagementProject/CustomersManagementProjectML.Model/ConsumeModel.cs
+++ CustomersManagementProject/CustomersManagementProjectML.Model/ConsumeModel.cs
@@ -27,7 +27,7 @@ namespace CustomersManagementProjectML.Model
             MLContext mlContext = new MLContext();
 
             // Load model & create prediction engine
-            string modelPath = @"C:\Users\Grunba\AppData\Local\Temp\MLVSTools\CustomersManagementProjectML\CustomersManagementProjectML.Model\MLModel.zip";
+            string modelPath = "MLModel.zip";
             ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
